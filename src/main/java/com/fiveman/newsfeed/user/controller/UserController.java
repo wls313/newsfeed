662ff6f23b2d308c1ce92c5c101f299180a1c661
gkdl4239 <gkdl4239@gmail.com>
2024-12-21 17:17:36 +0900

    feat : 유저 생성(POSTAPI) 추가
    /api/users 를 pre 디렉토리로 선언.
     클라이언트에게서 RequestBody 로 각각 이메일, 비밀번호, 나이를 입력받아 서비스에 전달하고 성공시 응답값과 CREATED 코드 반환

--- src/main/java/com/fiveman/newsfeed/user/controller/UserController.java
+++ src/main/java/com/fiveman/newsfeed/user/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.fiveman.newsfeed.user.controller;
 
+import com.fiveman.newsfeed.user.dto.SignupRequestDto;
+import com.fiveman.newsfeed.user.dto.UserDto;
+import com.fiveman.newsfeed.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserDto> create(@RequestBody SignupRequestDto requestDto) {
+
+        UserDto response = userService.create(requestDto);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+
 }
