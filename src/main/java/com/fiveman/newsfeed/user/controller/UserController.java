68b9b926c04a077d3f8129ff3619adb4050a149d
gkdl4239 <gkdl4239@gmail.com>
2024-12-22 12:42:36 +0900

    feat : 유저 단건 조회, 유저 전체 조회 추가
    단건 조회 : email을 url파라미터로 입력 받도록 하고 성공시 코드 200 반환
    전체 조회 : 페이징을 사용하여 url 파라미터로 볼 페이지와 페이지당 개수 입력받도록 하여 클라이언트에게 보여줌 추후 관리자만 이 기능을 이용할 수 있게 수정 해야할 듯

--- src/main/java/com/fiveman/newsfeed/user/controller/UserController.java
+++ src/main/java/com/fiveman/newsfeed/user/controller/UserController.java
@@ -4,12 +4,12 @@ import com.fiveman.newsfeed.user.dto.SignupRequestDto;
 import com.fiveman.newsfeed.user.dto.UserDto;
 import com.fiveman.newsfeed.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -26,5 +26,20 @@ public class UserController {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
+    @GetMapping("/email")
+    public ResponseEntity<UserDto> findByEmail(@RequestParam String email) {
 
+        UserDto response = userService.findByEmail(email);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserDto>> findAll(
+            @RequestParam(defaultValue = "10") int pageSize,
+            @RequestParam(defaultValue = "1") int pageNumber
+    ) {
+        Pageable pageable = PageRequest.of(pageNumber-1,pageSize);
+        return new ResponseEntity<>(userService.findAll(pageable), HttpStatus.OK);
+    }
 }
