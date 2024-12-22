66d549a627c0ffb4717eabfa4c0a6a151284d9d4
gkdl4239 <gkdl4239@gmail.com>
2024-12-22 12:43:29 +0900

    feat : 메서드 추가
    단건 조회시 이메일로 찾을수 있게끔 메서드 추가

--- src/main/java/com/fiveman/newsfeed/user/repository/UserRepository.java
+++ src/main/java/com/fiveman/newsfeed/user/repository/UserRepository.java
@@ -3,6 +3,11 @@ package com.fiveman.newsfeed.user.repository;
 import com.fiveman.newsfeed.common.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+
 }
