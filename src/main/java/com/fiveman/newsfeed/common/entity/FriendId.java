eb1f4c7731cdf930ffeb098817fa928a357ced6c
gkdl4239 <gkdl4239@gmail.com>
2024-12-22 14:19:42 +0900

    feat : Friend Entity 의 복합키인 FriendId 를 설계하는 클래스
    @Embeddable 은 Friend 엔티티의 @EmbeddedId 와 매핑됨. @EqualsAndHashCode, 즉 equals() 와 hashcode() 를 통해 엔티티 동등성을 확인
    매핑된 fromUserId 와 toUserId 를 묶은 복합키로 사용

---
+++ src/main/java/com/fiveman/newsfeed/common/entity/FriendId.java
@@ -0,0 +1,20 @@
+package com.fiveman.newsfeed.common.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class FriendId implements Serializable {
+
+    private Long fromUserId;
+    private Long toUserId;
+}
