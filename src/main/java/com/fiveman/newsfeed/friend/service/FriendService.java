638e0e567befe38b4228b98511a7d2e264605d19
gkdl4239 <gkdl4239@gmail.com>
2024-12-22 13:14:56 +0900

    feat : FriendService 생성
    구현해야할 친구 요청 메서드 추가

---
+++ src/main/java/com/fiveman/newsfeed/friend/service/FriendService.java
@@ -0,0 +1,9 @@
+package com.fiveman.newsfeed.friend.service;
+
+import com.fiveman.newsfeed.friend.dto.FriendRequestDto;
+
+
+public interface FriendService {
+
+    void create(FriendRequestDto request);
+}
