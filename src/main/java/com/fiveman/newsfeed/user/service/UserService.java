e6d2c76eb9b00980bc628bf72d641490c3df72d9
gkdl4239 <gkdl4239@gmail.com>
2024-12-22 12:44:17 +0900

    feat : 유저 서비스 인터페이스에 구현해야할 메서드 추가

--- src/main/java/com/fiveman/newsfeed/user/service/UserService.java
+++ src/main/java/com/fiveman/newsfeed/user/service/UserService.java
@@ -1,10 +1,20 @@
 package com.fiveman.newsfeed.user.service;
 
+import com.fiveman.newsfeed.common.entity.User;
 import com.fiveman.newsfeed.user.dto.SignupRequestDto;
 import com.fiveman.newsfeed.user.dto.UserDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 
 public interface UserService {
 
     UserDto create(SignupRequestDto request);
+
+    UserDto findByEmail(String email);
+
+    Page<UserDto> findAll(Pageable pageable);
+
+    User findById(Long id);
+
 }
