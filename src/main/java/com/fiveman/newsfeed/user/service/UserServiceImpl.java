d8449669319969e8d0e9c04795fa8385c1ef6415
gkdl4239 <gkdl4239@gmail.com>
2024-12-22 12:51:33 +0900

    feat : 단건 조회, 전체 조회, id조회 로직 추가
    단건 조회 : 이메일로 찾고 없으면 예외를 던지고 DB에서 꺼내온 User 객체를 정적 팩토리 메서드를 이용해 Dto로 변환하여 반환
    전체 조회 : Pageable 객체와 함께 전체를 조회한 후 찾아온 모든 값을 마찬가지로 Dto로 변환하여 반환
    id 조회 : FriendService에서 사용하기 위해 id로 조회하고 반환값이 User인 메서드 선언

--- src/main/java/com/fiveman/newsfeed/user/service/UserServiceImpl.java
+++ src/main/java/com/fiveman/newsfeed/user/service/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.fiveman.newsfeed.user.dto.SignupRequestDto;
 import com.fiveman.newsfeed.user.dto.UserDto;
 import com.fiveman.newsfeed.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -27,9 +29,29 @@ public class UserServiceImpl implements UserService{
         return UserDto.from(user);
     }
 
+    @Override
+    public UserDto findByEmail(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        return UserDto.from(user);
+    }
+
+    @Override
+    public Page<UserDto> findAll(Pageable pageable) {
+        return userRepository.findAll(pageable)
+                .map(UserDto::from);
+    }
+
     private void isDuplicateEmail(String email) {
         if(userRepository.existsByEmail(email)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
