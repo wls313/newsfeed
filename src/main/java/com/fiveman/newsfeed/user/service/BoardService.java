c90ff137e87d021afe245e33a0e35954f72ffe44
DeaHyun0911 <eogus0911@gmail.com>
2024-12-23 19:35:39 +0900

    refactor: createBoard refactor
    게시글 수정 기능 리팩토링

--- src/main/java/com/fiveman/newsfeed/user/service/BoardService.java
+++ src/main/java/com/fiveman/newsfeed/user/service/BoardService.java
@@ -5,31 +5,23 @@ import com.fiveman.newsfeed.common.entity.User;  // User 클래스 임포트
 import com.fiveman.newsfeed.user.dto.BoardRequestDto;
 import com.fiveman.newsfeed.user.repository.BoardRepository;
 import com.fiveman.newsfeed.user.repository.UserRepository;  // UserRepository 임포트
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class BoardService {
 
     private final BoardRepository boardRepository;
     private final UserRepository userRepository;  // UserRepository 의존성 추가
 
-    public BoardService(BoardRepository boardRepository, UserRepository userRepository) {
-        this.boardRepository = boardRepository;
-        this.userRepository = userRepository;
-    }
 
-    public Board createBoard(BoardRequestDto boardRequestDto, Long userId) {
+    public Board createBoard(Long id, String title, String contents) {
+
         // Board 객체 생성
-        Board board = new Board();
-        board.setTitle(boardRequestDto.getTitle());
-        board.setContent(boardRequestDto.getContent());
-
-        // userId로 User를 조회하여 Board에 설정
-        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-        board.setUser(user);  // User 객체를 Board에 설정
-
+        Board board = new Board(title, contents);
         return boardRepository.save(board);
     }
 
@@ -37,4 +29,6 @@ public class BoardService {
     public List<Board> getAllBoards() {
         return boardRepository.findAll();  // BoardRepository에서 findAll() 메서드를 호출하여 모든 게시물 반환
     }
+
+
 }
