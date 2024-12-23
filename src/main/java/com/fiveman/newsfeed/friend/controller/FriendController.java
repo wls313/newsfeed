4d6ee1ed6f3646f1ff5e2169e6847aecec79f8ca
gkdl4239 <gkdl4239@gmail.com>
2024-12-23 12:25:59 +0900

    feat : 친구 요청 조회 API 생성
    로그인을 구현하지 않아 일단 파라미터로 id를 받아서 조회 해오는 구조로 구현 추후 변경 예정

--- src/main/java/com/fiveman/newsfeed/friend/controller/FriendController.java
+++ src/main/java/com/fiveman/newsfeed/friend/controller/FriendController.java
@@ -1,14 +1,14 @@
 package com.fiveman.newsfeed.friend.controller;
 
+import com.fiveman.newsfeed.friend.dto.FriendResponseDto;
 import com.fiveman.newsfeed.friend.dto.FriendRequestDto;
 import com.fiveman.newsfeed.friend.service.FriendService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,6 +23,14 @@ public class FriendController {
         friendService.create(request);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/requests")
+    public ResponseEntity<List<FriendResponseDto>> findAllRequests(@RequestParam Long myId) {
+
+        List<FriendResponseDto> response = friendService.findAllRequest(myId);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
