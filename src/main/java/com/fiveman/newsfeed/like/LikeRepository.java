089fdf0343b638a3a897bb825408408c7b412afb
DeaHyun0911 <eogus0911@gmail.com>
2024-12-24 12:18:59 +0900

    feat: like repository add
    좋아요 레포지토리 추가

---
+++ src/main/java/com/fiveman/newsfeed/like/LikeRepository.java
@@ -0,0 +1,9 @@
+package com.fiveman.newsfeed.like;
+
+import com.fiveman.newsfeed.common.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, LikeId> {
+
+
+}
