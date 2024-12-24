5e58a650a3e4e10783391c465d4c561b0e889f38
ww313 <wlsghddl3358@naver.com>
2024-12-24 09:46:19 +0900

    @EnableJpaAuditing 이 2개 들어있던거 한개 삭제했습니다

--- src/main/java/com/fiveman/newsfeed/NewsfeedApplication.java
+++ src/main/java/com/fiveman/newsfeed/NewsfeedApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableJpaAuditing
 public class NewsfeedApplication {
 
     public static void main(String[] args) {
