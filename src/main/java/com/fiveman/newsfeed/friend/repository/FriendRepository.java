5256575f03f47aa891d8e388a6e2349f99b12171
gkdl4239 <gkdl4239@gmail.com>
2024-12-26 02:33:35 +0900

    feat : User 객체인 fromUser 와 toUser 로 Friend 튜플을 찾는 메서드 추가

--- src/main/java/com/fiveman/newsfeed/friend/repository/FriendRepository.java
+++ src/main/java/com/fiveman/newsfeed/friend/repository/FriendRepository.java
@@ -2,6 +2,7 @@ package com.fiveman.newsfeed.friend.repository;
 
 
 import com.fiveman.newsfeed.common.entity.Friend;
+import com.fiveman.newsfeed.common.entity.User;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -11,6 +12,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+
     @Query("SELECT f FROM Friend f WHERE f.toUser.userId = :toUserId")
     List<Friend> findAllRequest(@Param("toUserId") Long toUserId);
 
@@ -21,4 +23,5 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
     @Query("DELETE FROM Friend f WHERE f.fromUser.userId = :fromUserId AND f.toUser.userId = :toUserId")
     void deleteFriendByFromUserToUser(@Param("fromUserId") Long fromUserId ,@Param("toUserId") Long toUserId);
 
+    Friend findByFromUserAndToUser(User fromUser, User toUserId);
 }
