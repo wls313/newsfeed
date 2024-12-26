7ff99c30194996a4cf06a537813d867cd29b4ba4
gkdl4239 <gkdl4239@gmail.com>
2024-12-26 14:43:15 +0900

    fix : 유저 레포지토리 로직 변경
    isDeleted값이 1인 즉 삭제된 유저는 검색되지 않게끔 쿼리문 추가

--- src/main/java/com/fiveman/newsfeed/user/repository/UserRepository.java
+++ src/main/java/com/fiveman/newsfeed/user/repository/UserRepository.java
@@ -1,25 +1,22 @@
 package com.fiveman.newsfeed.user.repository;
 
 import com.fiveman.newsfeed.common.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
 
-    default User findByEmailOrElseThrow(String email) {
 
-        User user = findByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist email = " + email));
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.isDeleted = false")
+    Optional<User> findByEmail(@Param("email") String email);
 
-        if (user.isDeleted()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User with email = " + email + " is deleted.");
-        }
-
-        return user;
-    }
+    @Query("SELECT u FROM User u WHERE u.isDeleted = false")
+    Page<User> findAllUsers(Pageable pageable);
 
     boolean existsByEmail(String email);
 }
