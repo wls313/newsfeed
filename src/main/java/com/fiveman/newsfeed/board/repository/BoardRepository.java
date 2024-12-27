edabd626bdbab315ba99d572862083a974e07c8c
gkdl4239 <gkdl4239@gmail.com>
2024-12-27 10:55:22 +0900

    fix : 없는 게시글 예외처리 로직 변경

--- src/main/java/com/fiveman/newsfeed/board/repository/BoardRepository.java
+++ src/main/java/com/fiveman/newsfeed/board/repository/BoardRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -16,7 +18,7 @@ import java.util.NoSuchElementException;
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     default Board findByIdOrElseThrow(Long id) {
-        return findById(id).orElseThrow(() -> new NoSuchElementException("존재하지 않는 게시물입니다."));
+        return findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 게시물입니다."));
     }
 
 
